d7cab534b56307b388fa7f9c0c386036cb08bb61
Kiki Irfansyah <kikiirfansyah33@gmail.com>
2023-11-19 16:35:06 +0700

    Group & Dynamic Routes | 3

---
+++ src/app/product/[[...slug]]/page.tsx
@@ -0,0 +1,33 @@
+import { log } from "console";
+
+type DetailProductPageProps = {
+  params: {
+    slug: string[];
+  };
+};
+
+const DetailProductPage = (props: DetailProductPageProps) => {
+  const { params } = props;
+  return (
+    <div>
+      <h1> {params.slug ? "Detail Product Page" : "Product Page"}</h1>
+      {params.slug && (
+        <>
+          <p>Category : {params.slug[0]}</p>
+          <p>Gender : {params.slug[1]}</p>
+          <p>Id : {params.slug[2]}</p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default DetailProductPage;
+
+// export default function DetailProductPage(props: DetailProductPageProps) {
+//   return (
+//     <div>
+//       <h1>Detail Product Page : </h1>
+//     </div>
+//   );
+// }
