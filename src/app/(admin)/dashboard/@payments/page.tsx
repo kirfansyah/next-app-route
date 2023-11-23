5124597c3b669065e962efdbbc7c5fd731f7ec1c
Kiki Irfansyah <kikiirfansyah33@gmail.com>
2023-11-23 07:23:57 +0700

    Parallel Routes | 10

---
+++ src/app/(admin)/dashboard/@payments/page.tsx
@@ -0,0 +1,9 @@
+const Payments = () => {
+  return (
+    <div className="w-full h-96 bg-gray-300 rounded-[12px] flex justify-center items-center mt-5">
+      <h1>Payments</h1>
+    </div>
+  );
+};
+
+export default Payments;
---
+++ src/app/(admin)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const DasboardPage = () => {
+  return (
+    <div className="w-full h-96 bg-gray-300 rounded-[12px] flex justify-center items-center">
+      <h1>Dashboard</h1>
+    </div>
+  );
+};
+
+export default DasboardPage;
