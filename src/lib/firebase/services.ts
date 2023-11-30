14a85755ef9feba830342a2e3cc2f95f4caef7c3
Kiki Irfansyah <kikiirfansyah33@gmail.com>
2023-11-30 07:25:40 +0700

    Auth Register Part 1 (9:56) | 14

--- src/lib/firebase/services.ts
+++ src/lib/firebase/services.ts
@@ -1,11 +1,15 @@
 import {
+  addDoc,
   collection,
   doc,
   getDoc,
   getDocs,
   getFirestore,
+  query,
+  where,
 } from "firebase/firestore";
 import app from "./init";
+import bcrypt from "bcrypt";
 
 const firestore = getFirestore(app);
 
@@ -24,4 +28,35 @@ export async function retriveDataById(collectionName: string, id: string) {
   const snapshot = await getDoc(doc(firestore, collectionName, id));
   const data = snapshot.data();
   return data;
+}
+
+export async function register(
+  data: { fullname: string; email: string; password: string; role?: string },
+  callback: Function
+) {
+  const q = query(
+    collection(firestore, "users"),
+    where("email", "==", data.email)
+  );
+
+  const snapshot = await getDocs(q);
+  const users = snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+
+  if (users.length > 0) {
+    return callback({ status: false, message: "Email already exists" });
+  } else {
+    data.role = "admin";
+    data.password = await bcrypt.hash(data.password, 10);
+
+    await addDoc(collection(firestore, "users"), data)
+      .then(() => {
+        callback({ status: true, message: "User created successfully" });
+      })
+      .catch((error) => {
+        callback({ status: false, message: error.message });
+      });
+  }
 }
